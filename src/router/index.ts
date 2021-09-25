76f1de7a6cb569b6f411daffd9bc85606cbbf0ce
shopkeeper <734212732@qq.com>
2021-09-25 23:39:29 +0800

    feat(basic.vue): add basic.vue

--- src/router/index.ts
+++ src/router/index.ts
@@ -12,7 +12,13 @@ const routes: Array<RouteRecordRaw> = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "@/views/overview/overview.vue")
+      import(/* webpackChunkName: "overview" */ "@/views/overview/overview.vue")
+  },
+  {
+    path: "/basic",
+    name: "basic",
+    component: () =>
+      import(/* webpackChunkName: "basic" */ "@/views/basic/basic.vue")
   }
 ];
 
---
+++ src/views/basic/basic.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>basic</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
