cc700fb6ccbd19aa8bcfc89520165a9a8b645721
shopkeeper <734212732@qq.com>
2021-09-26 11:04:29 +0800

    chore: modify publicPath, install @element-plus/icons

--- vue.config.js
+++ vue.config.js
@@ -2,18 +2,7 @@ const path = require("path");
 module.exports = {
   // 配置方式一：使用VUE Cli提供的属性
   outputDir: "./build",
-  // devServer: {
-  //   proxy: {
-  //     "^/api": {
-  //       target: "http://152.136.185.210:5000",
-  //       pathRewrite: {
-  //         "^/api": ""
-  //       },
-  //       changeOrigin: true
-  //     }
-  //   }
-  // },
-  // 配置方式二：和webpack属性完全一致，最后会进行合并
+  publicPath: "/learning/ol-workshop",
   configureWebpack: {
     resolve: {
       alias: {
